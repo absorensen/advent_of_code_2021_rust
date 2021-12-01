888287fe3e68b946c4e26184bae89e106b51863c
Anders <anders-soerensen@hotmail.com>
2021-12-01 07:20:19 +0100

    Day 1 b

--- src/main.rs
+++ src/main.rs
@@ -7,25 +7,33 @@ fn main() -> std::io::Result<()> {
     file.read_to_string(&mut contents).unwrap();
 
     let mut count: i32 = 0;
+    let mut last_value: i32 = 0;
     let mut former_value: i32 = 0;
     let mut current_value: i32 = 0;
-    let mut first_loop: bool = true;
+    let mut next_value: i32 = 0;
+    let mut former_sum: i32 = 0;
+    let mut current_sum: i32 = 0;
+
+    let mut loop_counter: i32 = 0;
 
     for s in contents.lines() {
-        current_value = s.parse::<i32>().unwrap();
+        next_value = s.parse::<i32>().unwrap();
 
-        if first_loop {
-            first_loop = false;
+        if 2 < loop_counter {
+            former_sum = last_value + former_value + current_value;
+            current_sum = former_value + current_value + next_value;
+
+            if former_sum < current_sum {
+                count += 1;
+            } 
         }
-        else if former_value < current_value {
-            count += 1;
-        } 
-    
+        last_value = former_value;
         former_value = current_value;
-
+        current_value = next_value;
+        loop_counter += 1;
     }
 
-    println!("Successfully found {} positive deltas", count);
+    println!("Successfully found {} positive sum deltas", count);
     
     
     
