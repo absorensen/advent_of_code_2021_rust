6b5bd44260f74f547181df2cd01034ff399822db
Anders <anders-soerensen@hotmail.com>
2021-12-02 06:24:42 +0100

    Day 2, submitted

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ fn parse_txt_file_to_int_vec(path: &str) -> Vec<i32>{
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();
 
+    // Refactor to iterator
     let mut result : Vec<i32> = Vec::<i32>::new();
     for s in contents.lines() {
         result.push(s.parse::<i32>().unwrap());
@@ -14,8 +15,67 @@ fn parse_txt_file_to_int_vec(path: &str) -> Vec<i32>{
     result
 }
 
+fn parse_txt_file_to_str_tokens(path: &str) -> Vec<Vec<String>>{
+    let mut file = File::open(path).unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+
+    let mut result : Vec<Vec<String>> = Vec::<Vec<String>>::new();
+    for s in contents.lines() {
+        result.push(s.split(' ').map(|st| st.to_string()).collect());
+    }
+
+    result
+}
+
+fn b_02_21(use_functional: bool) -> i32 {
+    let commands = parse_txt_file_to_str_tokens("C:/Programming/advent_of_code_rust/input/day2.txt");
+
+    let mut depth: i32 = 0;
+    let mut horizontal_position: i32 = 0;
+    let mut aim: i32 = 0;
+    for command_index in 0..commands.len() {
+        let direction = &commands[command_index][0];
+        let magnitude = &commands[command_index][1].parse::<i32>().unwrap();
+
+        match direction.as_str() {
+            "forward" => {
+                horizontal_position += magnitude;
+                depth += aim * magnitude;
+            }
+            "down" => {aim += magnitude;}
+            "up" => {aim -= magnitude;}
+            _ => {}
+        }
+    }
+
+    println!("a_02_21: Horizontal Postion: {}  Depth: {}  Product: {}", horizontal_position, depth, horizontal_position * depth);
+    horizontal_position * depth
+}
+
+fn a_02_21(use_functional: bool) -> i32 {
+    let commands = parse_txt_file_to_str_tokens("C:/Programming/advent_of_code_rust/input/day2.txt");
+
+    let mut depth: i32 = 0;
+    let mut horizontal_position: i32 = 0;
+    for command_index in 0..commands.len() {
+        let direction = &commands[command_index][0];
+        let magnitude = &commands[command_index][1].parse::<i32>().unwrap();
+
+        match direction.as_str() {
+            "forward" => {horizontal_position += magnitude;}
+            "down" => {depth += magnitude;}
+            "up" => {depth -= magnitude;}
+            _ => {}
+        }
+    }
+
+    println!("a_02_21: Horizontal Postion: {}  Depth: {}  Product: {}", horizontal_position, depth, horizontal_position * depth);
+    horizontal_position * depth
+}
+
 fn b_01_21(use_functional: bool) -> usize {
-    let signal = parse_txt_file_to_int_vec("C:/Programming/advent_of_code_rust/input/day1a.txt");
+    let signal = parse_txt_file_to_int_vec("C:/Programming/advent_of_code_rust/input/day1.txt");
     
     if use_functional {
         let count = signal
@@ -48,7 +108,7 @@ fn b_01_21(use_functional: bool) -> usize {
 }
 
 fn a_01_21(use_functional: bool) -> usize{
-    let signal = parse_txt_file_to_int_vec("C:/Programming/advent_of_code_rust/input/day1a.txt");
+    let signal = parse_txt_file_to_int_vec("C:/Programming/advent_of_code_rust/input/day1.txt");
     
     if use_functional{
         let count = signal.windows(2).filter(|x| x[0] < x[1]).count();
@@ -70,12 +130,23 @@ fn a_01_21(use_functional: bool) -> usize{
 }
 
 fn main() -> std::io::Result<()> {
-    a_01_21(false);
-    b_01_21(false);    
+    a_02_21(false);
+    b_02_21(false);    
     
     Ok(())
 }
 
+#[test]
+fn test_b_02_21() {
+    assert_eq!(b_02_21(true), 1408487760);
+    assert_eq!(b_02_21(false), 1408487760);
+}
+
+#[test]
+fn test_a_02_21() {
+    assert_eq!(a_02_21(true), 1690020);
+    assert_eq!(a_02_21(false), 1690020);
+}
 
 #[test]
 fn test_b_01_21() {
